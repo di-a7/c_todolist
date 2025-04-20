f77734b1311bf4ffd69a8567804524ed3f675fac
diya <shresthadiya59@gmail.com>
2025-04-20 15:58:53 +0545

    admin customization

--- base/models.py
+++ base/models.py
@@ -5,3 +5,6 @@ class Todolist(models.Model):
    title = models.CharField(max_length=100)
    description = models.TextField()
    is_completed = models.BooleanField(default=False)
+   
+   def __str__(self):
+      return f"Title- {self.title}"
